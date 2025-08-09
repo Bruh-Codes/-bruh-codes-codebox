ee38c1c4fea13d31869a633bd0860ce9be965c18
Kamasah-Dickson <kamasahdickson19@gmail.com>
2025-08-09 16:09:40 +0000

    refactor: remove babel-loader from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,18 +13,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
         test: /\.css$/,
         use: [
           'style-loader',
